507354002cfc3cfa671c6ccc819c45291a4f24f5
Eric <154993587+ericbeaubrun@users.noreply.github.com>
2024-08-31 14:26:31 +0200

    scss fix

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/portfolio/',
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "@/styles/variables.scss";`
-            },
-        },
-    },
+    base: '/portfolio/'
 })
